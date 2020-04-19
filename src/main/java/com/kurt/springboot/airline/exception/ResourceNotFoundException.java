1ccf17edb44791879e69dfd718477aacd9c4e5da
HakanKurtt <hakan.kurtt@hotmail.com>
2020-04-19 12:20:54 +0300

    Add specific exception for http 404

---
+++ src/main/java/com/kurt/springboot/airline/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kurt.springboot.airline.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason= "Resource not found.")
+public class ResourceNotFoundException extends RuntimeException {}
